8eccb5619aea083bb9fee155f2b53c48b858a552
Aaron <aaron3993@hotmail.com>
2023-11-10 15:08:02 -0500

    added home page and logout button and functionality

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom
 // import Dashboard from './Dashboard';
 import Login from './Login/login';
 import Register from './Register/register';
+import Home from './Home/home';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" exact element={<Register />} />
+        <Route path="/signup" element={<Register/>}/>
           {/* <Route path="/dashboard" component={Dashboard} /> */}
           <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </div>
     </Router>
