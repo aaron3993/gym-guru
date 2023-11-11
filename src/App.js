31261c0d3acb6c73a2d6541521577e433c2c70f7
Aaron <aaron3993@hotmail.com>
2023-11-11 17:13:10 -0500

    created component folder, renamed component files, implemented private route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home/home';
-import Login from './Login/login';
-import Register from './Register/register';
-import Navbar from './Navbar/navbar';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Navbar from './components/Navbar/Navbar';
+import PrivateRoute from './components/PrivateRoute';
+import {auth} from './firebase';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 // import firebase from 'firebase/app';
 // import 'firebase/auth';
 
 const App = () => {
-  // const isAuthenticated = firebase.auth().currentUser !== null;
+  const [isAuthenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            console.log({user})
+            setAuthenticated(true);
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/auth.user
+          const uid = user.uid;
+        }
+      });
+  }, []);
 
   return (
     <Router>
       {/* <Navbar isAuthenticated={isAuthenticated} /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+          <PrivateRoute isAuthenticated={isAuthenticated}>
+            <Home />
+          </PrivateRoute>
+        } />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         {/* Other routes */}
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import firebase from 'firebase/app';
-// import 'firebase/auth';
 import {  signOut } from "firebase/auth";
-import {auth} from '../firebase';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {auth} from '../../firebase';
 
 
 const Navbar = () => {
-  const [isAuthenticated, setAuthenticated] = useState(false);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const auth = getAuth();
-
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            console.log({user})
-            setAuthenticated(true);
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/auth.user
-          const uid = user.uid;
-        }
-      });
-  }, []);
 
   const handleLogout = () => {
     signOut(auth).then(() => {
@@ -40,11 +22,9 @@ const Navbar = () => {
       <Link to="/" style={{ color: 'white', marginRight: '20px' }}>
         Home
       </Link>
-      {isAuthenticated && (
         <button onClick={handleLogout} style={{ background: 'transparent', border: 'none', color: 'white', cursor: 'pointer' }}>
           Logout
         </button>
-      )}
     </nav>
   );
 };
