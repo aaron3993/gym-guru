51f08d344b2a2a72020e875bcbcbea447802416a
Aaron <aaron3993@hotmail.com>
2023-12-03 23:23:27 -0500

    able to retrieve workout in db based on id and display the name

--- src/pages/WorkoutsPage/WorkoutList/WorkoutCard/WorkoutDetail/WorkoutDetailPage.js
+++ src/pages/WorkoutsPage/WorkoutList/WorkoutCard/WorkoutDetail/WorkoutDetailPage.js
@@ -1,17 +1,42 @@
-// WorkoutDetailPage.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../../../../firebase';
+import LoadingSpinner from '../../../../../components/LoadingSpinner';
 
 const WorkoutDetailPage = () => {
   const { workoutId } = useParams();
+  const [selectedWorkout, setSelectedWorkout] = useState(null);
 
-  // Fetch the workout details based on workoutId
+  useEffect(() => {
+    const fetchWorkoutById = async () => {
+      try {
+        const workoutDoc = doc(db, 'workouts', workoutId);
+        const workoutSnapshot = await getDoc(workoutDoc);
+
+        if (workoutSnapshot.exists()) {
+          setSelectedWorkout({ id: workoutSnapshot.id, ...workoutSnapshot.data() });
+        } else {
+          console.error('Workout not found');
+        }
+      } catch (error) {
+        console.error('Error fetching workout:', error);
+      }
+    };
+
+    fetchWorkoutById();
+  }, [workoutId]);
+
+  if (!selectedWorkout) {
+    return <LoadingSpinner />
+  }
+
+  const { name } = selectedWorkout;
 
   return (
     <div>
       <h1>Workout Details</h1>
-      <p>Workout ID: {workoutId}</p>
-      {/* Display other workout details */}
+      <p>Workout Name: {name}</p>
     </div>
   );
 };
