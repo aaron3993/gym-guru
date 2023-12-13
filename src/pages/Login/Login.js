643ff985283269f426f217c7153031a6ba10bb72
Aaron <aaron3993@hotmail.com>
2023-12-13 01:08:14 -0500

    added redirect to home from signup, set debugger to local host 3000, and added prettier config

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,78 +1,73 @@
-import React, {useState} from 'react';
-import {  signInWithEmailAndPassword   } from 'firebase/auth';
-import { auth } from '../../firebase';
-import { NavLink, useNavigate } from 'react-router-dom'
-import "./Login.css"
- 
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./Login.css";
+
 const Login = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-       
-    const onLogin = (e) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            navigate("/")
-            console.log(user);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage)
-        });
-       
-    }
- 
-    return(
-        <>
-            <main>
-                <section className='login-layout'>
-                    <div className="login-container">
-                        <p className='title'>Gym Guru</p>
-                        <form>
-                            <div>
-                            <label htmlFor="email-address">Email address</label>
-                            <input
-                                id="email-address"
-                                name="email"
-                                type="email"
-                                required
-                                placeholder="Email address"
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                            </div>
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-                            <div>
-                            <label htmlFor="password">Password</label>
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                required
-                                placeholder="Password"
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            </div>
+  const onLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigate("/");
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
 
-                            <div>
-                            <button onClick={onLogin}>Login</button>
-                            </div>
-                        </form>
+  return (
+    <>
+      <main>
+        <section className="login-layout">
+          <div className="login-container">
+            <p className="title">Gym Guru</p>
+            <form>
+              <div>
+                <label htmlFor="email-address">Email address</label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="Email address"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
 
-                        <p className="text-sm text-white text-center">
-                            No account yet?{' '}
-                            <NavLink to="/register">
-                            Sign up
-                            </NavLink>
-                        </p>
-                    </div>
-                </section>
-            </main>
-        </>
-    )
-}
- 
-export default Login
+              <div>
+                <label htmlFor="password">Password</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <button onClick={onLogin}>Login</button>
+              </div>
+            </form>
+
+            <p className="text-sm text-white text-center">
+              No account yet? <NavLink to="/register">Sign up</NavLink>
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Login;
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,51 +1,52 @@
-import React, { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { collection, addDoc } from 'firebase/firestore';
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { auth, db } from '../../firebase';
-import './Register.css';
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { collection, addDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db } from "../../firebase";
+import "./Register.css";
 
 const Register = () => {
   const navigate = useNavigate();
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      // Validate inputs before proceeding
+      console.log("hi");
       if (!name || !email || !password) {
-        alert('Please fill in all required fields.');
+        alert("Please fill in all required fields.");
         return;
       }
 
       if (!isValidEmail(email)) {
-        alert('Please enter a valid email address.');
+        alert("Please enter a valid email address.");
         return;
       }
 
       if (password.length < 6) {
-        alert('Password must be at least 6 characters long.');
+        alert("Password must be at least 6 characters long.");
         return;
       }
 
-      // Proceed with registration if all validations pass
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
 
       await updateProfile(user, { displayName: name });
 
-      const docRef = await addDoc(collection(db, 'users'), {
+      await addDoc(collection(db, "users"), {
         uid: user.uid,
         name: name,
         email: user.email,
       });
-
-      console.log('Document written with ID: ', docRef.id);
-      navigate('/login');
+      navigate("/");
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -53,9 +54,7 @@ const Register = () => {
     }
   };
 
-  // Function to validate email format
   const isValidEmail = (email) => {
-    // Use a simple regex for email validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
@@ -67,7 +66,6 @@ const Register = () => {
           <div>
             <p className="title"> Gym Guru </p>
             <form>
-              {/* Styling for the "Name" input field */}
               <div>
                 <label htmlFor="name" className="signup-container-label">
                   Name
@@ -82,9 +80,11 @@ const Register = () => {
                 />
               </div>
 
-              {/* Styling for the "Email" input field */}
               <div>
-                <label htmlFor="email-address" className="signup-container-label">
+                <label
+                  htmlFor="email-address"
+                  className="signup-container-label"
+                >
                   Email address
                 </label>
                 <input
@@ -94,11 +94,10 @@ const Register = () => {
                   onChange={(e) => setEmail(e.target.value)}
                   required
                   placeholder="Email address"
-                  pattern="[^\s@]+@[^\s@]+\.[^\s@]+" // Add pattern for email validation
+                  pattern="[^\s@]+@[^\s@]+\.[^\s@]+"
                 />
               </div>
 
-              {/* Styling for the "Password" input field */}
               <div>
                 <label htmlFor="password" className="signup-container-label">
                   Password
@@ -110,18 +109,21 @@ const Register = () => {
                   onChange={(e) => setPassword(e.target.value)}
                   required
                   placeholder="Password"
-                  minLength="6" // Add minlength for password length validation
+                  minLength="6"
                 />
               </div>
 
-              <button type="submit" onClick={onSubmit} className="signup-container-button">
+              <button
+                type="submit"
+                onClick={onSubmit}
+                className="signup-container-button"
+              >
                 Sign up
               </button>
             </form>
 
             <p>
-              Already have an account?{' '}
-              <NavLink to="/login">Sign in</NavLink>
+              Already have an account? <NavLink to="/login">Sign in</NavLink>
             </p>
           </div>
         </div>
