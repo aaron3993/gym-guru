353f3e9338c66979be0c359b95c14d3a698f1cfe
Aaron <aaron3993@hotmail.com>
2023-12-20 13:22:49 -0500

    added editable reps and sets states and inputs

--- src/pages/WorkoutsPage/WorkoutList/WorkoutCard/WorkoutDetail/ExerciseRow/ExerciseRow.js
+++ src/pages/WorkoutsPage/WorkoutList/WorkoutCard/WorkoutDetail/ExerciseRow/ExerciseRow.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExerciseRow.css";
 
-const ExerciseRow = ({ exercise, onRemoveExercise }) => {
+const ExerciseRow = ({ exercise, onRemoveExercise, onUpdateExercise }) => {
+  const [editableReps, setEditableReps] = useState(exercise.reps);
+  const [editableSets, setEditableSets] = useState(exercise.sets);
+
+  const handleRepsChange = (e) => {
+    const newReps = e.target.value;
+    setEditableReps(newReps);
+  };
+
+  const handleSetsChange = (e) => {
+    const newSets = e.target.value;
+    setEditableSets(newSets);
+  };
+
+  const handleUpdateExercise = () => {
+    onUpdateExercise({
+      ...exercise,
+      reps: editableReps,
+      sets: editableSets,
+    });
+  };
+
   return (
     <div className="exercise-row">
       <div className="exercise-info">
         <img src={exercise.gifUrl} alt={exercise.name} />
         <h3>{exercise.updatedName}</h3>
-        <h3>Reps: {exercise.reps}</h3>
-        <h3>Sets: {exercise.sets}</h3>
+        <label>
+          Reps:
+          <input
+            type="number"
+            value={editableReps}
+            onChange={handleRepsChange}
+          />
+        </label>
+        <label>
+          Sets:
+          <input
+            type="number"
+            value={editableSets}
+            onChange={handleSetsChange}
+          />
+        </label>
       </div>
       <button onClick={() => onRemoveExercise(exercise)}>Remove</button>
+      <button onClick={handleUpdateExercise}>Update</button>
     </div>
   );
 };
