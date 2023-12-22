acf8ef12524eb9a10273cc990e5a40caa3d09ef6
Aaron <aaron3993@hotmail.com>
2023-12-22 01:28:55 -0500

    only updated reps and sets when number has changed within range and displaying success message

--- src/pages/WorkoutsPage/WorkoutList/WorkoutCard/WorkoutDetail/ExerciseRow/ExerciseRow.js
+++ src/pages/WorkoutsPage/WorkoutList/WorkoutCard/WorkoutDetail/ExerciseRow/ExerciseRow.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { InputNumber, Alert } from "antd";
+import { InputNumber, Alert, message } from "antd";
 import "./ExerciseRow.css";
 import { updateExerciseInWorkout } from "../../../../../../utils/firestoreUtils";
 
@@ -28,21 +28,24 @@ const ExerciseRow = ({
     if (editableReps < 1 || editableReps > 100) {
       return setRepsError("Reps must be between 1 and 100");
     }
-    setRepsError(null);
-    updateExerciseInWorkout(
-      workoutId,
-      exercise.id,
-      editableReps,
-      exercise.sets
-    );
-    setIsEditingReps(false);
-    onUpdateExercise();
+    if (editableReps !== exercise.reps) {
+      setRepsError(null);
+      updateExerciseInWorkout(
+        workoutId,
+        exercise.id,
+        editableReps,
+        exercise.sets
+      );
+      onUpdateExercise();
+      message.success("Reps updated successfully!");
+    }
   };
 
   const handleUpdateSets = () => {
     if (editableSets < 1 || editableSets > 10) {
-      setSetsError("Sets must be between 1 and 10");
-    } else {
+      return setSetsError("Sets must be between 1 and 10");
+    }
+    if (editableSets !== exercise.sets) {
       setSetsError(null);
       updateExerciseInWorkout(
         workoutId,
@@ -50,8 +53,8 @@ const ExerciseRow = ({
         exercise.reps,
         editableSets
       );
-      setIsEditingSets(false);
       onUpdateExercise();
+      message.success("Sets updated successfully!");
     }
   };
 
@@ -61,34 +64,36 @@ const ExerciseRow = ({
         <img src={exercise.gifUrl} alt={exercise.name} />
         <h3>{exercise.updatedName}</h3>
         <div className="editable-number">
+          <span>Reps: </span>
           {isEditingReps ? (
             <InputNumber
               autoFocus
               value={editableReps}
               onChange={(value) => setEditableReps(value)}
               onBlur={handleUpdateReps}
+              style={{ width: "60px" }}
             />
           ) : (
-            <div>
-              <span>Reps: </span>
-              <span onClick={handleEditReps}>{editableReps}</span>
-            </div>
+            <span onClick={handleEditReps} className="clickable-number">
+              {editableReps}
+            </span>
           )}
         </div>
         {repsError && <Alert message={repsError} type="error" showIcon />}
         <div className="editable-number">
+          <span>Sets: </span>
           {isEditingSets ? (
             <InputNumber
               autoFocus
               value={editableSets}
               onChange={(value) => setEditableSets(value)}
               onBlur={handleUpdateSets}
+              style={{ width: "60px" }}
             />
           ) : (
-            <div>
-              <span>Sets: </span>
-              <span onClick={handleEditSets}>{editableSets}</span>
-            </div>
+            <span onClick={handleEditSets} className="clickable-number">
+              {editableSets}
+            </span>
           )}
         </div>
         {setsError && <Alert message={setsError} type="error" showIcon />}
