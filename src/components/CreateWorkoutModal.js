daba60301227a35844b0822c1b6175d34b5dabf1
Aaron <aaron3993@hotmail.com>
2023-12-29 00:41:40 -0500

    using antd modal and added validation for creating workout

--- src/components/CreateWorkoutModal.js
+++ src/components/CreateWorkoutModal.js
@@ -1,33 +1,82 @@
 import React, { useState } from "react";
-import Modal from "react-modal";
+import Modal from "antd/lib/modal/Modal";
+import { Input, Button, Form, Typography } from "antd";
 import "./SharedModal.css";
 
+const { Text } = Typography;
+
 const CreateWorkoutModal = ({ isOpen, onRequestClose, onCreateWorkout }) => {
-  const [workoutName, setWorkoutName] = useState("");
+  const [form] = Form.useForm();
+  const [error, setError] = useState("");
 
   const handleCreateWorkout = () => {
-    onCreateWorkout(workoutName);
-    onRequestClose();
+    const workoutName = form.getFieldValue("workoutName");
+
+    if (!workoutName || workoutName.trim().length === 0) {
+      return;
+    }
+
+    form
+      .validateFields()
+      .then((values) => {
+        const validatedWorkoutName = values?.workoutName;
+
+        if (
+          validatedWorkoutName.length < 1 ||
+          validatedWorkoutName.length > 100
+        ) {
+          setError("Workout name must be between 1 and 100 characters.");
+          return;
+        }
+
+        onCreateWorkout(validatedWorkoutName);
+        form.resetFields();
+        onRequestClose();
+      })
+      .catch((error) => {
+        console.error("Form validation error:", error);
+        setError(
+          "An error occurred while validating the form. Please try again."
+        );
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const workoutName = e.target.value;
+    setError("");
+    form.setFieldsValue({ workoutName });
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleCreateWorkout();
+    }
   };
 
   return (
     <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      className="modal"
-      overlayClassName="ReactModal__Overlay"
-      contentLabel="Create Workout Modal"
-      shouldCloseOnOverlayClick={true}
+      title="Create a New Workout"
+      open={isOpen}
+      onCancel={onRequestClose}
+      footer={[
+        <Button key="cancel" onClick={onRequestClose}>
+          Cancel
+        </Button>,
+        <Button key="create" type="primary" onClick={handleCreateWorkout}>
+          Create
+        </Button>,
+      ]}
     >
-      <h1>Create a New Workout</h1>
-      <input
-        type="text"
-        value={workoutName}
-        onChange={(e) => setWorkoutName(e.target.value)}
-        placeholder="Workout Name"
-      />
-      <button onClick={handleCreateWorkout}>Create</button>
-      <button onClick={onRequestClose}>Cancel</button>
+      <Form form={form}>
+        <Form.Item label="Workout Name" name="workoutName">
+          <Input
+            placeholder="Workout Name"
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          />
+        </Form.Item>
+        {error && <Text type="danger">{error}</Text>}
+      </Form>
     </Modal>
   );
 };
