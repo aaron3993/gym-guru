06fcb30691d38ee4bf1732853b074d1936c48f10
Aaron <aaron3993@hotmail.com>
2024-11-28 17:06:36 -0500

    replaced form with buttons for manual and ai workout creations and styled them

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-scroll";
-import GoalsForm from "../../components/GoalsForm/GoalsForm";
 import "./Home.css";
 
 const Home = () => {
-  const navbarHeight = window.innerHeight * 0.08; // Adjusting for the navbar height
+  const navigate = useNavigate();
+  const navbarHeight = window.innerHeight * 0.08;
+
+  const handleManualWorkout = () => {
+    navigate("/workouts");
+  };
 
   return (
     <div className="home-page">
@@ -14,7 +19,7 @@ const Home = () => {
           <h1 className="app-title">Gym Guru</h1>
           <p className="slogan">Customize Your Journey, Sculpt Your Success.</p>
           <Link
-            to="form-section"
+            to="options-section"
             smooth={true}
             duration={800}
             offset={-navbarHeight}
@@ -26,9 +31,42 @@ const Home = () => {
         </div>
       </section>
 
-      {/* Render the form as a separate component */}
-      <section id="form-section" className="full-page-section">
-        <GoalsForm />
+      <section id="options-section" className="full-page-section">
+        <div className="options-container">
+          <h2>Choose Your Workout Creation Method</h2>
+          <div className="button-options">
+            <div className="option">
+              <Button
+                type="primary"
+                className="manual-button"
+                onClick={handleManualWorkout}
+              >
+                Create Workouts Manually
+              </Button>
+              <p className="option-note">
+                Customize your workouts by selecting exercises, reps, and sets.
+                Perfect for those who prefer a hands-on approach.
+              </p>
+            </div>
+            <div className="option">
+              <Button
+                type="primary"
+                className="ai-button"
+                // onClick={() =>
+                //   document
+                //     .getElementById("ai-form")
+                //     .scrollIntoView({ behavior: "smooth" })
+                // }
+              >
+                Generate with AI
+              </Button>
+              <p className="option-note">
+                Use our AI tool to create a personalized program quickly. Just
+                fill out the form, and we'll handle the rest!
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
