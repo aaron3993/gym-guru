020dd0da500bda4e7aefd40ac6730a7a4bb1dc2b
Aaron <aaron3993@hotmail.com>
2024-12-12 15:24:43 -0500

    added loading spinner to routinesPage

--- src/pages/RoutinesPage/RoutinesPage.js
+++ src/pages/RoutinesPage/RoutinesPage.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import RoutineList from "./RoutineList/RoutineList";
 import { getAllRoutinesForUser } from "../../utils/firestoreUtils";
 import { useAuth } from "../../contexts/AuthContext";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 const RoutinesPage = () => {
   const { user } = useAuth();
   const [routines, setRoutines] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchRoutines = async () => {
@@ -15,6 +17,8 @@ const RoutinesPage = () => {
         setRoutines(routinesData);
       } catch (error) {
         console.error("Error fetching routines:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -24,7 +28,9 @@ const RoutinesPage = () => {
   return (
     <div className="routines-page">
       <h1>Your Routines</h1>
-      {routines.length > 0 ? (
+      {loading ? (
+        <LoadingSpinner />
+      ) : routines.length > 0 ? (
         <RoutineList routines={routines} />
       ) : (
         <p>No routines found. Start creating one!</p>
