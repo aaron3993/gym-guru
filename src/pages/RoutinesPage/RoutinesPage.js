eb76b1eee17b5d5eb412961da8d154e5be0d27b5
Aaron <aaron3993@hotmail.com>
2024-12-13 01:13:52 -0500

    added button to open modal to generate routine on routines page

--- src/pages/RoutinesPage/RoutinesPage.js
+++ src/pages/RoutinesPage/RoutinesPage.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "antd";
 import RoutineList from "./RoutineList/RoutineList";
 import { getAllRoutinesForUser } from "../../utils/firestoreUtils";
 import { useAuth } from "../../contexts/AuthContext";
 import LoadingSpinner from "../../components/LoadingSpinner";
+import GenerateWorkoutModal from "../../components/GenerateWorkoutModal";
 import "./RoutinesPage.css";
 
 const RoutinesPage = () => {
   const { user } = useAuth();
   const [routines, setRoutines] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     const fetchRoutines = async () => {
       try {
         const routinesData = await getAllRoutinesForUser(user.uid);
-        console.log(routinesData);
         setRoutines(routinesData);
       } catch (error) {
         console.error("Error fetching routines:", error);
@@ -26,11 +28,22 @@ const RoutinesPage = () => {
     fetchRoutines();
   }, [user.uid]);
 
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <div className="routines-page">
       <div className="header">
         <h1>Your Routines</h1>
         <p>View your AI-generated routines.</p>
+        <Button type="primary" className="ai-button" onClick={showModal}>
+          Generate Routine with AI
+        </Button>
       </div>
       <div className="content">
         {loading ? (
@@ -41,6 +54,7 @@ const RoutinesPage = () => {
           <p>No routines found. Start creating one!</p>
         )}
       </div>
+      <GenerateWorkoutModal isVisible={isModalVisible} onClose={closeModal} />
     </div>
   );
 };
