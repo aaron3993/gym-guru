96ebf8c712ee3f1ab2de36875c83d89dbb961203
Aaron <aaron3993@hotmail.com>
2024-12-20 17:23:11 -0500

    swapped options in home page, increased sizing

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -41,6 +41,16 @@ const Home = () => {
           <h2>Choose Your Workout Creation Method</h2>
           <div className="button-options">
             <div className="option">
+              <Button type="primary" className="ai-button" onClick={showModal}>
+                Generate with AI
+              </Button>
+              <p className="option-note">
+                Use our AI tool to create a personalized weekly routine. All you
+                need to do is fill out a quick form. You just need to wait about
+                a minute and we'll notify you when it's ready.
+              </p>
+            </div>
+            <div className="option">
               <Button
                 type="primary"
                 className="manual-button"
@@ -52,15 +62,6 @@ const Home = () => {
                 Browse and filter through our large library of exercises to
                 create your own workouts. Perfect for those who prefer a
                 hands-on approach.
-              </p>
-            </div>
-            <div className="option">
-              <Button type="primary" className="ai-button" onClick={showModal}>
-                Generate with AI
-              </Button>
-              <p className="option-note">
-                Use our AI tool to create a personalized weekly routine. Just
-                fill out the form, and we'll handle the rest!
               </p>
             </div>
           </div>
