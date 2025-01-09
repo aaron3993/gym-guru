0f8cd4fa4a61b1e9b00854918d159ddaf7ae270d
Aaron <aaron3993@hotmail.com>
2025-01-08 23:55:56 -0500

    replaced config variables with env

--- src/firebase.js
+++ src/firebase.js
@@ -4,28 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics, logEvent } from "firebase/analytics";
 import "firebase/firestore";
 
-const environment = process.env.NODE_ENV || "production";
-
-const firebaseConfig =
-  environment === "production"
-    ? {
-        apiKey: "AIzaSyAS4_3SkAK1nz-x3RGeszlX-BxPZt_0BsQ",
-        authDomain: "gymguru-37ed9.firebaseapp.com",
-        projectId: "gymguru-37ed9",
-        storageBucket: "gymguru-37ed9.appspot.com",
-        messagingSenderId: "586710732433",
-        appId: "1:586710732433:web:ee2bb9cef1d3af75fd3930",
-        measurementId: "G-3QB6JVYJS5",
-      }
-    : {
-        apiKey: "AIzaSyCnwF_xUgPgfoyQ4lw46Q31OnnJOwN3p9U",
-        authDomain: "gym-guru-staging.firebaseapp.com",
-        projectId: "gym-guru-staging",
-        storageBucket: "gym-guru-staging.appspot.com",
-        messagingSenderId: "304940808094",
-        appId: "1:304940808094:web:72cf821435d2a304f1088f",
-        measurementId: "G-NLV7ZDK4X0",
-      };
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
