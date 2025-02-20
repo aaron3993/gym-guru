341d25d47eedd597f31c614749c61da69705a8c9
Aaron <aaron3993@hotmail.com>
2025-02-19 20:58:17 -0500

    refined prompt

--- src/services/openaiUtils.js
+++ src/services/openaiUtils.js
@@ -52,33 +52,35 @@ export const generateWorkoutPlan = async (criteria, userId, jobId) => {
       throw new Error("Failed to format exercise names for input");
     }
 
-    const prompt = `Create a structured JSON format for a weekly workout plan for someone whose goal is ${
-      criteria.goal
-    } and fitness level is ${criteria.fitnessLevel}.
-    Only output the JSON and no other text.
-    Use the following exercises and their gifUrls in your plan:
-    
-    ${exerciseNamesString}
-    
-    The format should include:
-    - title: ${criteria.fitnessLevel} ${formatGoalsAndFitnessLevelsText(
+    const prompt = `
+      Create a structured JSON format for a weekly workout plan for someone whose goal is ${
+        criteria.goal
+      } and fitness level is ${criteria.fitnessLevel}.
+      Only output the JSON and no other text.
+      Use the following exercises in your plan:
+      
+      ${exerciseNamesString}
+      
+      The format should include:
+      - title: ${criteria.fitnessLevel} ${formatGoalsAndFitnessLevelsText(
       criteria.goal
     )} Routine (Capitalize first letters)
-    - fitnessLevel
-    - goal
-    - days. Each day has the following attributes:
-      - day: e.g., "Monday"
-      - dayOfWeek: e.g., 1
-      - name: Muscle group trained
-      - exercises: An array of five exercises per weight training day. Each exercise should have the following attributes:
-        - id: Empty string
-        - name: The name of the exercise in lowercase
-        - gifUrl: The gif URL of the exercise
-        - sets: The number of sets (integer)
-        - reps: A string to represent the range of reps (e.g., "8-12")
-        - rest: The rest duration (e.g., "60 seconds")
-      - Rest days should have an empty exercises array.
-      - Include cardio where necessary, even on rest days if appropriate.`;
+      - fitnessLevel
+      - goal
+      - days. Each day has the following attributes:
+        - day: e.g., "Monday"
+        - dayOfWeek: e.g., 1
+        - name: Muscle group trained
+        - exercises: An array of 4-7 exercises per weight training day. Each exercise should have the following attributes:
+          - id: Empty string
+          - name: The name of the exercise in lowercase
+          - gifUrl: Empty string
+          - sets: The number of sets (integer)
+          - reps: A string to represent the range of reps (e.g., "8-12")
+          - rest: The rest duration (e.g., "60 seconds")
+        - Rest days should have an empty exercises array.
+        - Include cardio where necessary.
+      `;
 
     const token = await fetchAuthToken();
 
